e8fd055ea0c91854b60ab9eb3daf59e00a00846f
Rinoto Harto <rinoto.harto@phincon.com>
2021-11-24 02:40:45 +0700

    development phase 2

--- src/components/Started/index.jsx
+++ src/components/Started/index.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import _ from 'lodash';
+
+import pigura from '../../static/images/pigura.png';
 import classes from './style.module.scss';
 
-const Started = ({ openInvitation }) => {
+const Started = ({ openInvitation, name }) => {
   return (
     <div className={classes.container}>
-      <h1>Started Page</h1>
-      <button onClick={openInvitation}>Buka Undangan</button>
+      <div className={classes.wraper}>
+      <img src={pigura} alt="frame" />
+      </div>
+      {!_.isEmpty(name) &&
+        <div className={classes.toWraper}>
+      <p>Teruntuk</p>
+      <p className={classes.to}>{name}</p>
+      <p>Di tempat</p>
+      </div>
+      }
+      <div className={classes.btn} onClick={openInvitation}><strong>Buka Undangan</strong></div>
     </div>
   );
 }
--- src/containers/InvitationPage/index.jsx
+++ src/containers/InvitationPage/index.jsx
@@ -1,24 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import _ from 'lodash';
-import { useLocation } from 'react-router';
+import { useLocation } from 'react-router-dom';
 
 import StartedComponent from '../../components/Started';
 import AudioComponent from '../../components/AudioPlayer';
 import Azmi from '../../static/images/azmi.png';
+import Ridwan from '../../static/images/ridwan.png';
 import Male from '../../static/images/male.png';
 import Female from '../../static/images/female.png';
 import WingTop from '../../static/images/wing-top.png';
 import WingBottom from '../../static/images/wing-bottom.png';
 import ThirdImage from '../../static/images/thirdimage.png';
 import Frame from '../../static/images/frame.png';
+import wingg from '../../static/images/wingg.png';
+import topevent from '../../static/images/topevent.png';
+import calender from '../../static/icons/calender.png';
+import time from '../../static/icons/time.png';
+import Location from '../../static/icons/location.png';
+import plane from '../../static/icons/plane.png';
 import classes from './style.module.scss';
 
 const InvitationPage = () => {
   const [isInvitationOpen, setIsInvitationOpen] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
   const location = useLocation();
-  let name = location.search.split('=')[1];
-  name = name.split('+').join(' ')
+  let name = location?.search?.split('=')[1];
+  name = name?.split('+').join(' ');
 
   const calculateTimeLeft = () => {
     let year = new Date().getFullYear();
@@ -69,6 +75,10 @@ const InvitationPage = () => {
     setIsPlaying(!isPlaying);
   }
 
+  const goToMaps = () => {
+    window.open('https://goo.gl/maps/mwWn37GwhiCRmrw6A', '_blank');
+  }
+
   const generateHeader = () => {
     return (
       <div className={classes.header}>
@@ -87,14 +97,21 @@ const InvitationPage = () => {
   const generateStory = () => {
     return (
       <div className={classes.storySection}>
-        <h1>Story</h1>
+        <div className={classes.storyWrapper}>
+          <div className={classes.story}>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget malesuada quam placerat sed tellus nulla pellentesque. Integer non, pharetra mattis amet, amet.</p>
+          </div>
+        </div>
+        <div className={classes.imageWrapper}>
+          <img src={Ridwan} alt='Brides'/>
+        </div>
       </div>
     );
   }
-
+  
   const generateSecondStory = () => {
     return (
-      <div className={classes.seconStoryContainer}>
+      <div className={classes.secondStoryContainer}>
         <div className={classes.imageWrapper}>
           <img src={Azmi} alt="Brides" />
         </div>
@@ -105,6 +122,39 @@ const InvitationPage = () => {
         </div>
       </div>
     );
+  }
+  
+
+  const secondImage = () => {
+    return (
+      <div className={classes.paralaxx}>
+        <div className={classes.paralaxxWraper}>
+        </div>
+      </div>
+    );
+  }
+
+  const summary = () => {
+    return (
+      <div className={classes.summary}>
+        <div className={classes.summaryWraper}>
+          <div className={classes.title}>
+            <p>
+              Cerita Kita
+            </p>
+          </div>
+            <p className={classes.summary}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi mattis sagittis aliquet <br/>
+              volutpat arcu lorem amet. Nibh pellentesque feugiat est, sed augue sit et. Diam mi, <br/>
+              nisi, neque senectus et. Mauris, imperdiet sodales magna nibh odio scelerisque<br/>
+              dapibus purus tellus. Velit mi pellentesque diam cursus nam varius. Ornare<br/>
+              sagittis, amet, non ultricies. Aliquam non amet mauris mattis nisi. Lacus metus,<br/> 
+              elit morbi mattis vulputate faucibus amet.
+            </p>
+            <img src={wingg} alt='wingBottom'/>
+        </div>
+      </div>
+    )
   }
 
   const generateBridesProfile = () => {
@@ -131,6 +181,54 @@ const InvitationPage = () => {
         </div>
         <div className={classes.wingWrapper}>
           <img className={classes.image} src={WingBottom} alt="wing" />
+        </div>
+      </div>
+    );
+  }
+
+  const eventDetail = () => {
+    return (
+      <div className={classes.event}>
+        <img className={classes.topEvent} src={topevent} alt='top' />
+        <div className={classes.greeting}>
+          <p>
+            Assalamu'alaikum Warahmatullahi Wabarakatuh<br/><br/>
+            Maha Suci Allah yang telah menciptakan makhluk-Nya berpasang-pasangan.<br/>
+            Ya Allah semoga ridho-Mu tercurah mengiringi pernikahan kamu
+          </p>
+        </div>
+        <div className={classes.details}>
+          <div className={classes.titleWraper}>
+            <p className={classes.title}>AKAD & RESEPSI</p>
+          </div>
+          <div className={classes.calender}>
+            <img src={calender} alt='calender' />
+            <p>
+              MINGGU 09 JANUARI 2022  
+            </p>
+          </div>
+          <div className={classes.timesWraper}>
+            <div className={classes.time}>
+              <img src={time} alt='time' />
+              <p>PUKUL 08.00 WIB -09.00 WIB</p>
+            </div>
+            <div className={classes.time}>
+              <img src={time} alt='time' />
+              <p>
+                PUKUL 08.00 WIB - 09.00 WIB
+              </p>
+            </div>
+          </div>
+          <div className={classes.locationWraper}>
+            <img src={Location} alt='location' />
+            <p>
+              JL. CISUNGSANG BANTEN                
+            </p>
+          </div>
+          <div onClick={goToMaps} className={classes.btnmap}>
+            <p>Menuju Lokasi</p>
+            <img src={plane} alt='gotomap' />
+          </div>
         </div>
       </div>
     );
@@ -176,8 +274,12 @@ const InvitationPage = () => {
     return (
       <div className={classes.invitationContainer}>
         {generateHeader()}
+        {generateStory()}
         {generateSecondStory()}
+        {secondImage()}
+        {summary()}
         {generateBridesProfile()}
+        {eventDetail()}
         {generatePoemSection()}
         <AudioComponent isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
       </div>
@@ -185,7 +287,7 @@ const InvitationPage = () => {
   }
   return (
     <div className={classes.container}>
-      {!isInvitationOpen ? <StartedComponent openInvitation={openInvitation} /> : generateInvitation()}
+      {!isInvitationOpen ? <StartedComponent openInvitation={openInvitation} name={name} /> : generateInvitation()}
     </div>
   )
 }
