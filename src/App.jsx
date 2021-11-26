26aa4f14e4093337ac8ab07bf3fbf213a3a176a3
Rinoto Harto <rinoto.harto@phincon.com>
2021-11-27 00:07:40 +0700

    fix popup prokes size

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import InvitationPage from './containers/InvitationPage';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -7,16 +7,6 @@ import store from "./store";
 const App = () => {
 
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <AudioPlayer isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
-    //   </header>
-    //   <p>{wording}</p>
-    //   <button onClick={copyText}>Copy Text</button>
-    //   <p>{notif}</p>
-    //   <InvitationPage />
-    //   <StartedPage />
-    // </div>
     <Provider store={store}>
       <Router>
         <Routes>
