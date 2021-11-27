d08a2828179899815651199bc08606a07b7bc8e9
Rinoto Harto <rinoto.harto@phincon.com>
2021-11-28 01:28:11 +0700

    create gift confirmation form

--- src/sagas.js
+++ src/sagas.js
@@ -1,12 +1,22 @@
 import { call, put, takeLatest } from "redux-saga/effects";
 import _ from "lodash";
-import { GET_ALL_GUEST, POST_REGISTRATION } from "./store/constants/index";
-import { getAllGuest, postRegistration } from "./domain/API";
+import {
+  GET_ALL_GUEST,
+  POST_REGISTRATION,
+  POST_GIFT_CONFIRMATION,
+} from "./store/constants/index";
+import {
+  getAllGuest,
+  postRegistration,
+  postGiftConfirmation,
+} from "./domain/API";
 import {
   setGuests,
   setMessages,
   setNewGuest,
   setErrorPost,
+  setConfirmationError,
+  setConfirmationSuccess,
 } from "./store/actions";
 
 function* doGetAllGuest() {
@@ -33,7 +43,21 @@ function* doPostRegistration({ guestData }) {
   }
 }
 
+function* doGiftConfirmation({ userData }) {
+  try {
+    const confirmationResponse = yield call(postGiftConfirmation, userData);
+    if (confirmationResponse) {
+      yield put(setConfirmationSuccess());
+    }
+  } catch (error) {
+    if (error.response.status === 400) {
+      yield put(setConfirmationError(error.response.data.message));
+    }
+  }
+}
+
 export default function* mySaga() {
   yield takeLatest(GET_ALL_GUEST, doGetAllGuest);
   yield takeLatest(POST_REGISTRATION, doPostRegistration);
+  yield takeLatest(POST_GIFT_CONFIRMATION, doGiftConfirmation);
 }
--- src/store/reducers/invitationReducer.js
+++ src/store/reducers/invitationReducer.js
@@ -5,12 +5,18 @@ import {
   SET_NEW_GUEST,
   SET_ERROR_POST,
   RESET_ERROR_POST,
+  SET_CONFIRMATION_ERROR,
+  RESET_CONFIRMATION_ERROR,
+  SET_CONFIRMATION_SUCCESS,
+  RESET_CONFIRMATION_SUCCESS,
 } from "../constants/index";
 
 export const initialState = {
   guests: [],
   messages: [],
   isError: false,
+  confirmationErrorMessage: "",
+  confirmationSuccess: false,
 };
 
 const invitationState = (state = initialState, action) =>
@@ -32,6 +38,18 @@ const invitationState = (state = initialState, action) =>
         break;
       case RESET_ERROR_POST:
         draft.isError = false;
+        break;
+      case SET_CONFIRMATION_ERROR:
+        draft.confirmationErrorMessage = action.message;
+        break;
+      case RESET_CONFIRMATION_ERROR:
+        draft.confirmationErrorMessage = "";
+        break;
+      case SET_CONFIRMATION_SUCCESS:
+        draft.confirmationSuccess = true;
+        break;
+      case RESET_CONFIRMATION_SUCCESS:
+        draft.confirmationSuccess = false;
         break;
       default:
         return state;
