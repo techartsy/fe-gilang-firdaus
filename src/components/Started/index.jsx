54cba539df12c32c08583cb38decc2a6e4378150
techartsy <techartsy.indonesia@gmail.com>
2021-11-30 12:01:08 +0700

    mobile finishing

--- src/components/Started/index.jsx
+++ src/components/Started/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import _ from 'lodash';
 import pigura from '../../static/images/pigura.png';
+import piguraSm from '../../static/images/pigura-sm.png';
+import useWindowDimension from '../../utils/useWindowDimensions';
 import classes from './style.module.scss';
 
 const Started = ({ openInvitation, name }) => {
+  const { width } = useWindowDimension();
+
   return (
     <div className={classes.container}>
       <div className={classes.wraper}>
-        <img src={pigura} alt="frame" />
+        <img src={width === 'lg' ? pigura : piguraSm} alt="frame" />
       </div>
       {!_.isEmpty(name) &&
         <div className={classes.toWraper}>
