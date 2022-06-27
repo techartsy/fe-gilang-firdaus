8b64389f680df9dc51c630b5300c8256f4ee592d
brajasifa11 <brajasifa11@gmail.com>
2022-06-27 23:51:34 +0700

    section early

--- src/components/Started/index.jsx
+++ src/components/Started/index.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import _ from 'lodash';
-import pigura from '../../static/images/pigura.png';
-import piguraSm from '../../static/images/pigura-sm.png';
-import useWindowDimension from '../../utils/useWindowDimensions';
+import piguraTop from '../../static/images/piguraTop.png';
+import gunungan from '../../static/images/pigura.png';
+import piguraBottom from '../../static/images/piguraBottom.png';
 import classes from './style.module.scss';
 
 const Started = ({ openInvitation, name }) => {
-  const { width } = useWindowDimension();
 
   return (
     <div className={classes.container}>
       <div className={classes.wraper}>
-        <img src={width === 'lg' ? pigura : piguraSm} alt="frame" />
+        <img className={classes.pigura} src={piguraTop} alt="frame" />
+        <img className={classes.top} src={gunungan} alt="frame" />
+        {!_.isEmpty(name) &&
+          <div className={classes.toWraper}>
+            <p>Teruntuk</p>
+            <p className={classes.to}>{name}</p>
+            <p>Di tempat</p>
+          </div>
+        }
+        <div className={classes.btn} onClick={openInvitation}><strong>Buka Undangan</strong></div>
+        <img className={classes.bottom} src={piguraBottom} alt="frame" />
       </div>
-      {!_.isEmpty(name) &&
-        <div className={classes.toWraper}>
-          <p>Teruntuk</p>
-          <p className={classes.to}>{name}</p>
-          <p>Di tempat</p>
-        </div>
-      }
-      <div className={classes.btn} onClick={openInvitation}><strong>Buka Undangan</strong></div>
     </div>
   );
 }
